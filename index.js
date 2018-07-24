3d678abec42e8447021e49042929e470b5099338
Daniel James Stephenson <dan_0502@hotmail.co.uk>
2018-07-24 17:33:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ function yourAnimal() {
   return animal
 }
 
-function add2(n, two) {
+function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
